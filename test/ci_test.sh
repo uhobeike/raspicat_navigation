58710f846f50e8ce1bcb2c5fd28550bc785f9755
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-09 01:58:54 +0900

    Fix ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xve
-DOCKER_CONTAINER_ID=$(docker ps -a | grep "/bin/bash" | awk '{print $1}')
+DOCKER_CONTAINER_ID=$(docker ps -a | grep "ubeike/raspicat-ros1-melodic-navigation" | awk '{print $1}')
 
 docker exec -it $DOCKER_CONTAINER_ID /bin/bash -c \
     "xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' roslaunch turtlebot3_gazebo turtlebot3_world.launch;
