ec4d64469e67278118b6b25aa83ace43b2fa42ca
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-09 21:04:28 +0900

    Fix ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -2,6 +2,7 @@
 DOCKER_CONTAINER_ID=$(docker ps -a | grep "ubeike/raspicat-ros1-melodic-navigation" | awk '{print $1}')
 docker start $DOCKER_CONTAINER_ID
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c \
-    "source ~/.bashrc;
-    (xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' roscore&);
+    "source /opt/ros/melodic/setup.bash;
+    (xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' roscore&); 
+    sleep 5;
     rostopic list"
