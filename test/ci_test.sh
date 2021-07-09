79a23669e3fc65257364d2e7f6cdee6af9e4c926
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-09 22:21:34 +0900

    Test ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -2,8 +2,8 @@
 DOCKER_CONTAINER_ID=$(docker ps -a | grep "ubeike/raspicat-ros1-melodic-navigation" | awk '{print $1}')
 docker start $DOCKER_CONTAINER_ID
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c \
-    "source /opt/ros/melodic/setup.bash;
-    (xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' roscore&); 
-    sleep 5;
+    "source /ros_entrypoint.sh;
+    (xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' roslaunch turtlebot3_gazebo turtlebot3_world.launch &); 
+    sleep 10;
     rostopic list;
     killall rosmaster"
