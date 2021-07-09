1f6033d64859b989b5b5d1fb675582afed599e59
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-09 12:05:13 +0900

    Test ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -2,4 +2,5 @@
 DOCKER_CONTAINER_ID=$(docker ps -a | grep "ubeike/raspicat-ros1-melodic-navigation" | awk '{print $1}')
 docker start $DOCKER_CONTAINER_ID
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c \
-    "xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' ls"
+    "source /opt/ros/melodic/setup.bash;
+    xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' roscore&"
