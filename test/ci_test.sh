af3358f1fa2040f29f52deb8e8c5b203f1ff2306
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-09 22:26:41 +0900

    Test ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -3,6 +3,7 @@ DOCKER_CONTAINER_ID=$(docker ps -a | grep "ubeike/raspicat-ros1-melodic-navigati
 docker start $DOCKER_CONTAINER_ID
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c \
     "source /ros_entrypoint.sh;
+    source /home/catkin_ws/devel/setup.bash;
     (xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' roslaunch turtlebot3_gazebo turtlebot3_world.launch &); 
     sleep 10;
     rostopic list;
