9b69c338b019bb348048ccaf7bedd800b0e68393
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-09 22:41:23 +0900

    Fix ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -xve
-DOCKER_CONTAINER_ID=$(docker ps -a | grep "ubeike/raspicat-ros1-melodic-navigation" | awk '{print $1}')
+DOCKER_CONTAINER_ID=$(docker ps -a | grep "ros_entrypoint.sh" | awk '{print $1}')
 docker start $DOCKER_CONTAINER_ID
 docker exec $DOCKER_CONTAINER_ID /bin/bash -c \
     "source /ros_entrypoint.sh;
     source /home/catkin_ws/devel/setup.bash;
+    export TURTLEBOT3_MODEL=burger;
     (xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' roslaunch turtlebot3_gazebo turtlebot3_world.launch &); 
     sleep 10;
     rostopic list;
