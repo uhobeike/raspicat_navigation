3c229921dfcf8187d02e44cb72e01cfcfd60df67
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-09 22:52:05 +0900

    Fix ci

--- .github/environment_ci.sh
+++ .github/environment_ci.sh
@@ -3,12 +3,6 @@ cd ..
 
 docker run \
     -v $(pwd):/home/catkin_ws/src \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -e DISPLAY=$DISPLAY \
-    -e QT_X11_NO_MITSHM=1 \
-    -e LIBGL_ALWAYS_INDIRECT=1 \
-    --privileged \
-    --net=host \
 ubeike/raspicat-ros1-melodic-navigation /bin/bash -c \
     "git clone -b melodic-devel https://github.com/ROBOTIS-GIT/turtlebot3.git;
     git clone -b melodic-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git;
