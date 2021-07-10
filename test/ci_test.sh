d3c80c1e04bc649d24ff3f43562578a400e54bdc
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-10 22:04:32 +0900

    Test ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -8,38 +8,38 @@ docker exec $DOCKER_CONTAINER_ID /bin/bash -c \
     (xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' roslaunch turtlebot3_gazebo turtlebot3_world.launch &); 
     sleep 10;
     (xvfb-run --auto-servernum  -s '-screen 0 1400x900x24' roslaunch raspicat_navigation ci_test.launch &);
-    sleep 10;
-    rostopic list;
-(rostopic pub -1 /move_base/goal move_base_msgs/MoveBaseActionGoal "header: \
-  seq: 0 \
-  stamp: \
-    secs: 0 \
-    nsecs: 0 \
-  frame_id: '' \
-goal_id: \
-  stamp: \
-    secs: 0 \
-    nsecs: 0 \
-  id: '' \
-goal: \
-  target_pose: \
-    header: \
-      seq: 0 \
-      stamp: \
-        secs: 0 \
-        nsecs: 0 \
-      frame_id: 'map' \
-    pose: \
-      position: \
-        x: 1.0 \
-        y: 0.0 \
-        z: 0.0 \
-      orientation: \
-        x: 0.0 \
-        y: 0.0 \
-        z: 0.5 \
-        w: 1.0");
-        killall rosmaster &"
+    sleep 10"
+#     rostopic list;
+# (rostopic pub -1 /move_base/goal move_base_msgs/MoveBaseActionGoal "header: \
+#   seq: 0 \
+#   stamp: \
+#     secs: 0 \
+#     nsecs: 0 \
+#   frame_id: '' \
+# goal_id: \
+#   stamp: \
+#     secs: 0 \
+#     nsecs: 0 \
+#   id: '' \
+# goal: \
+#   target_pose: \
+#     header: \
+#       seq: 0 \
+#       stamp: \
+#         secs: 0 \
+#         nsecs: 0 \
+#       frame_id: 'map' \
+#     pose: \
+#       position: \
+#         x: 1.0 \
+#         y: 0.0 \
+#         z: 0.0 \
+#       orientation: \
+#         x: 0.0 \
+#         y: 0.0 \
+#         z: 0.5 \
+#         w: 1.0");
+#         killall rosmaster &"
     # while true; do sleep 1 | rostopic echo -n 1 /move_base/feedback | grep -A 10 pose; done &;
     
     # bash -c "timeout 40 echo $(rostopic echo -n 1 /move_base/result | grep "Goal reached")| sed 's/^.*"\(.*\)".*$/\1/';
