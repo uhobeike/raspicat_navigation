6148f4504561098e4f6c84ac20c59aedba3cfc4f
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-10 23:18:10 +0900

    Test ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -7,8 +7,6 @@ docker exec $DOCKER_CONTAINER_ID /bin/bash -c \
     export TURTLEBOT3_MODEL=burger;
     (xvfb-run --auto-servernum -s '-screen 0 1400x900x24' roslaunch turtlebot3_gazebo turtlebot3_world.launch &); 
     sleep 10;
-    (ls &);
-    sleep 10;
     rostopic list"
 # (rostopic pub -1 /move_base/goal move_base_msgs/MoveBaseActionGoal "header: \
 #   seq: 0 \
