0aaf38bfdf1ce56372e9ac4914cfcf09ea1e3bd3
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-11 14:08:19 +0900

    Test ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -5,7 +5,7 @@ docker exec $DOCKER_CONTAINER_ID /bin/bash -c \
     "source /ros_entrypoint.sh;
     source /home/catkin_ws/devel/setup.bash;
     export TURTLEBOT3_MODEL=burger;
-    (xvfb-run --auto-servernum -s "-screen 0 1400x900x24" roslaunch turtlebot3_gazebo turtlebot3_world.launch &); 
+    (xvfb-run --auto-servernum -s '-screen 0 1400x900x24' roslaunch turtlebot3_gazebo turtlebot3_world.launch &); 
     sleep 15;
     rostopic list;
     rostopic list;
