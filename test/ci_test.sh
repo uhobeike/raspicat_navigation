762c9aa99f07afc4c69466269afebec496913375
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-12 11:56:13 +0900

    Test ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -12,7 +12,6 @@ else
     source /home/catkin_ws/devel/setup.bash
     export TURTLEBOT3_MODEL=burger
     xvfb-run --auto-servernum -s "-screen 0 1400x900x24" roslaunch turtlebot3_gazebo turtlebot3_world.launch &
-    watch -n 0.1 free -g
     while true; do sleep 0.1 && free -g; done
     sleep 15
     killall rosmaster &
