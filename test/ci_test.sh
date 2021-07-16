d7ec6ae9fe374b3bdd881fa580d9e0ec4d97088d
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-16 12:44:16 +0900

    Test ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -14,6 +14,7 @@ else
     xvfb-run --auto-servernum -s "-screen 0 1400x900x24" roslaunch turtlebot3_gazebo turtlebot3_world.launch &
     # while true; do sleep 0.1 && free -g; done
     sleep 15
+    exec "$@"
     # killall rosmaster &
 fi
 # source /home/catkin_ws/devel/setup.bash;
