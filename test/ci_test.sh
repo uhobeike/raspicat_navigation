c72a1b0f103bf080e4636bf3d6eecb5ae31c9ee9
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-17 13:32:26 +0900

    Test ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -1,57 +1,45 @@
 #!/bin/bash -xve
 
-# DOCKER_CONTAINER_ID=$(docker ps -a | grep "ros_entrypoint.sh" | awk '{print $1}')
-# if [ $(whoami) = "runner" ]; then
-#     DOCKER_CONTAINER_ID=$(docker ps -a | grep "ros_entrypoint.sh" | awk '{print $1}')
-#     docker start $DOCKER_CONTAINER_ID
+xvfb-run --auto-servernum -s "-screen 0 1400x900x24" roslaunch turtlebot3_gazebo turtlebot3_world.launch &
+sleep 10
 
-#     docker exec $DOCKER_CONTAINER_ID /bin/bash -c \
-#         "./raspicat_navigation/test/ci_test.sh"
-# else
-    xvfb-run --auto-servernum -s "-screen 0 1400x900x24" roslaunch turtlebot3_gazebo turtlebot3_world.launch &
-    sleep 300
-    # killall rosmaster &
-# fi
-# source /home/catkin_ws/devel/setup.bash;
-# export TURTLEBOT3_MODEL=burger;
-# (xvfb-run --auto-servernum -s '-screen 0 1400x900x24' roslaunch turtlebot3_gazebo turtlebot3_world.launch &); 
-# sleep 15;
-# killall rosmaster &;
-# exit 0"
+xvfb-run --auto-servernum -s '-screen 0 1400x900x24' roslaunch raspicat_navigation ci_test.launch &;
+sleep 10
 
-# (rostopic pub -1 /move_base/goal move_base_msgs/MoveBaseActionGoal "header: \
-#   seq: 0 \
-#   stamp: \
-#     secs: 0 \
-#     nsecs: 0 \
-#   frame_id: '' \
-# goal_id: \
-#   stamp: \
-#     secs: 0 \
-#     nsecs: 0 \
-#   id: '' \
-# goal: \
-#   target_pose: \
-#     header: \
-#       seq: 0 \
-#       stamp: \
-#         secs: 0 \
-#         nsecs: 0 \
-#       frame_id: 'map' \
-#     pose: \
-#       position: \
-#         x: 1.0 \
-#         y: 0.0 \
-#         z: 0.0 \
-#       orientation: \
-#         x: 0.0 \
-#         y: 0.0 \
-#         z: 0.5 \
-#         w: 1.0");
-#         killall rosmaster &"
-    # while true; do sleep 1 | rostopic echo -n 1 /move_base/feedback | grep -A 10 pose; done &;
-    
-    # bash -c "timeout 40 echo $(rostopic echo -n 1 /move_base/result | grep "Goal reached")| sed 's/^.*"\(.*\)".*$/\1/';
-    # if [ $? -eq 0 ];then echo "a"; break;else echo "b";break;fi"
-    
-    # if [ $? -eq 0 ];then killall rosmaster; break;else exit 1 | exit 1;break;fi
+rostopic pub -1 /move_base/goal move_base_msgs/MoveBaseActionGoal "header: \
+  seq: 0 \
+  stamp: \
+    secs: 0 \
+    nsecs: 0 \
+  frame_id: '' \
+goal_id: \
+  stamp: \
+    secs: 0 \
+    nsecs: 0 \
+  id: '' \
+goal: \
+  target_pose: \
+    header: \
+      seq: 0 \
+      stamp: \
+        secs: 0 \
+        nsecs: 0 \
+      frame_id: 'map' \
+    pose: \
+      position: \
+        x: 1.0 \
+        y: 0.0 \
+        z: 0.0 \
+      orientation: \
+        x: 0.0 \
+        y: 0.0 \
+        z: 0.5 \
+        w: 1.0");
+
+exit 0
+# while true; do sleep 1 | rostopic echo -n 1 /move_base/feedback | grep -A 10 pose; done &;
+
+# bash -c "timeout 40 echo $(rostopic echo -n 1 /move_base/result | grep "Goal reached")| sed 's/^.*"\(.*\)".*$/\1/';
+# if [ $? -eq 0 ];then echo "a"; break;else echo "b";break;fi"
+
+# if [ $? -eq 0 ];then killall rosmaster; break;else exit 1 | exit 1;break;fi
