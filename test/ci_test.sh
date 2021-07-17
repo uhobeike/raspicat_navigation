ccd0e1465fc0b35eaa59664967061ab8c15080ff
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-07-17 14:08:29 +0900

    Test ci

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -6,35 +6,35 @@ sleep 10
 xvfb-run --auto-servernum -s '-screen 0 1400x900x24' roslaunch raspicat_navigation ci_test.launch &
 sleep 10
 
-rostopic pub -1 /move_base/goal move_base_msgs/MoveBaseActionGoal "header: \
-  seq: 0 \
-  stamp: \
-    secs: 0 \
-    nsecs: 0 \
-  frame_id: '' \
-goal_id: \
-  stamp: \
-    secs: 0 \
-    nsecs: 0 \
-  id: '' \
-goal: \
-  target_pose: \
-    header: \
-      seq: 0 \
-      stamp: \
-        secs: 0 \
-        nsecs: 0 \
-      frame_id: 'map' \
-    pose: \
-      position: \
-        x: 1.0 \
-        y: 0.0 \
-        z: 0.0 \
-      orientation: \
-        x: 0.0 \
-        y: 0.0 \
-        z: 0.5 \
-        w: 1.0";
+rostopic pub -1 /move_base/goal move_base_msgs/MoveBaseActionGoal "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: ''
+goal_id:
+  stamp:
+    secs: 0
+    nsecs: 0
+  id: ''
+goal:
+  target_pose:
+    header:
+      seq: 0
+      stamp:
+        secs: 0
+        nsecs: 0
+      frame_id: 'map'
+    pose:
+      position:
+        x: 1.0
+        y: 0.0
+        z: 0.0
+      orientation:
+        x: 0.0
+        y: 0.0
+        z: 0.5
+        w: 1.0"
 
 exit 0
 # while true; do sleep 1 | rostopic echo -n 1 /move_base/feedback | grep -A 10 pose; done &;
