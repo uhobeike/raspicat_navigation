61ec4c3df23e97895adaf207498e66b864cc5956
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-11-16 15:35:23 +0900

    Add ros::AsyncSpinner

--- src/WaypointNav_node.cpp
+++ src/WaypointNav_node.cpp
@@ -1,33 +1,39 @@
 /*
-*Copyright 2021, Tatsuhiro Ikebe.
-*
-*Licensed under the Apache License, Version 2.0 (the "License");
-*you may not use this file except in compliance with the License.
-*You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-*Unless required by applicable law or agreed to in writing, software
-*distributed under the License is distributed on an "AS IS" BASIS,
-*WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*See the License for the specific language governing permissions and
-*limitations under the License.
-*/
+ *Copyright 2021, Tatsuhiro Ikebe.
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
+ */
 
 #include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
 
 #include "raspicat_navigation/WaypointNav.hpp"
 
-using namespace::std;
-
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "WaypointNav");
-	ros::NodeHandle nh;
+  ros::init(argc, argv, "WaypointNav");
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
 
-	waypoint_nav::WaypointNav wv(nh, ros::this_node::getName(), argv[1]);
-	ROS_INFO("%s: Please ' rostopic pub  -1 /goal_command std_msgs/String go ' ",
-			 ros::this_node::getName().c_str());
-	ros::spin();
-	return 0;
+  // tf2_ros::Buffer buffer(ros::Duration(10));
+  // tf2_ros::TransformListener tf(buffer);
+
+  waypoint_nav::WaypointNav wv(nh, ros::this_node::getName(), argv[1]);
+  ROS_INFO("%s: Please ' rostopic pub  -1 /goal_command std_msgs/String go ' ",
+           ros::this_node::getName().c_str());
+
+  ros::AsyncSpinner spinner(pnh.param("num_callback_threads", 1));
+  spinner.start();
+  ros::waitForShutdown();
+  return 0;
 }
