a1d4b12b1ec9543f47f426421aaafd0d7e356e41
uhobeike <beike315@icloud.com>
2022-06-04 17:46:39 +0000

    Fix

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -32,7 +32,7 @@ timeout 300 rostopic echo -n 1 /waypoint_goal_function
 # Printf result
 if [ $? -eq 0 ];then 
   killall ffmpeg
-  sleep 5
+  sleep 30
   killall rosmaster
   printf '\033[42m%s\033[m\n' 'Docker Test SUCCEED'
   exit 0
