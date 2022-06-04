7a4b6ebd74cfe98432f5481b35f7259a76d802f1
Tatsuhiro Ikebe <beike315@icloud.com>
2022-06-04 15:57:57 +0900

    Update (#149)
    * Update
    
    * Fix
    
    * Fix
    
    * Package Change
    
    * Add dependent
    
    * Update test
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -5,69 +5,31 @@ top -n 1 -b | head -n 20
 while true; do sleep 10 | top -n 1 -b | head -n 20; done &
 
 # Gazebo launch
-xvfb-run --auto-servernum -s "-screen 0 1400x900x24" roslaunch turtlebot3_gazebo turtlebot3_world.launch &
-sleep 10
+roslaunch raspicat_navigation raspicat_tsudanuma_2_19_world.launch \
+  x_gazebo:=0.155971128532 y_gazebo:=-0.0254326737864 yaw_gazebo:=0 open_gui:=false &
+sleep 20
 
-# Rviz|Navigation launch
-xvfb-run --auto-servernum -s '-screen 0 1400x900x24' roslaunch raspicat_navigation ci_test.launch &
-sleep 10
+# Rviz & Navigation launch
+xvfb-run --auto-servernum -s "-screen 0 1400x900x24" roslaunch raspicat_navigation ci_test.launch \
+  mcl:=amcl waypoint_yaml_file:=$(rospack find raspicat_navigation)/test/waypoint.yaml \
+  map_name:=tsudanuma_2_19 open_rviz:=true &
+sleep 60
 
-# 2D-PoseEstimate publish
-rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
-  seq: 0
-  stamp:
-    secs: 0
-    nsecs: 0
-  frame_id: 'map'
-pose:
-  pose:
-    position: {x: -1.862, y: -0.621, z: 0.0}
-    orientation: {x: 0.0, y: 0.0, z: 0.03052, w: 0.99953}"
-sleep 5 
+# Execute start operation
+rostopic pub -1 /way_nav_start std_msgs/Empty
 
-# MoveBaseGoal publish
-rostopic pub -1 /move_base/goal move_base_msgs/MoveBaseActionGoal "header:
-  seq: 0
-  stamp:
-    secs: 0
-    nsecs: 0
-  frame_id: ''
-goal_id:
-  stamp:
-    secs: 0
-    nsecs: 0
-  id: ''
-goal:
-  target_pose:
-    header:
-      seq: 0
-      stamp:
-        secs: 0
-        nsecs: 0
-      frame_id: 'map'
-    pose:
-      position:
-        x: 0.658029079437
-        y: -0.549129128456
-        z: 0.0
-      orientation:
-        x: 0.0
-        y: 0.0
-        z: -0.0150406081599
-        w: 0.999886883655"
+# Execute restart operation
+timeout 300 rostopic echo -n 1 /waypoint_stop_function;rostopic pub -1 /way_nav_restart std_msgs/Empty
 
-# Check robot motion planning
-while true; do sleep 1 | rostopic echo -n 1 /move_base/feedback | grep -A 10 pose; done &
-
-# Check goal status
-timeout 40 echo $(rostopic echo -n 1 /move_base/result | grep "Goal reached")| sed 's/^.*"\(.*\)".*$/\1/'
+# Check goal
+timeout 300 rostopic echo -n 1 /waypoint_goal_function
 
 # Printf result
 if [ $? -eq 0 ];then 
   killall rosmaster
-  printf '\033[42m%s\033[m\n' 'DOCKER TEST SUCCEED'
+  printf '\033[42m%s\033[m\n' 'Docker Test SUCCEED'
   exit 0
 else
-  printf '\033[31m%s\033[m\n' 'DOCKER TEST FAILED'
+  printf '\033[31m%s\033[m\n' 'Docker Test FAILED'
   exit 1
 fi
