5f4ff2e930488df613f3f6d8281a7adbc76178fa
uhobeike <beike315@icloud.com>
2022-06-04 05:38:46 +0000

    Fix

--- test/ci_test.sh
+++ test/ci_test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -xve
 
-source /root/.bashrc
-
 # Check ros node process
 top -n 1 -b | head -n 20
 while true; do sleep 10 | top -n 1 -b | head -n 20; done &
@@ -9,12 +7,12 @@ while true; do sleep 10 | top -n 1 -b | head -n 20; done &
 # Gazebo launch
 xvfb-run --auto-servernum -s "-screen 0 1400x900x24" roslaunch raspicat_navigation raspicat_tsudanuma_2_19_world.launch \
   x_gazebo:=0.155971128532 y_gazebo:=-0.0254326737864 yaw_gazebo:=0 &
-sleep 10
+sleep 20
 
 # Rviz & Navigation launch
 xvfb-run --auto-servernum -s '-screen 0 1400x900x24' roslaunch raspicat_navigation ci_test.launch \
   mcl:=amcl waypoint_yaml_file:=$(rospack find raspicat_navigation)/test/waypoint.yaml &
-sleep 10
+sleep 30
 
 # Check robot motion planning
 while true; do sleep 1 | rostopic echo -n 1 /move_base/feedback | grep -A 10 pose; done &
